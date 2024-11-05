ac99aa64e1ed3f1fe65d220dee23a80480623011
Uli <ulidots@ulidots.de>
2024-11-05 21:48:57 +0100

    Code quality

--- src/amerre.py
+++ src/amerre.py
@@ -1,8 +1,12 @@
 """ Main file """
-
+import logging
 import os
 
 from app.startup import Startup
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
-Startup().setup(root_dir).start()
+
+try:
+    Startup().setup(root_dir).start()
+except KeyboardInterrupt as e:
+    logging.debug(e)
