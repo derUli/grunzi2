feba2e5f2faf13b8908e6cc3c34d5c0f45772540
Uli <ulidots@ulidots.de>
2024-11-05 22:02:59 +0100

    Code quality

---
+++ src/duties.py
@@ -0,0 +1,25 @@
+from duty import duty
+from duty.context import Context
+
+
+@duty
+def translations(ctx: Context):
+    """ Update translation """
+
+    ctx.run('pybabel extract . -o resources/locales/base.po')
+    ctx.run('pybabel update -i resources/locales/base.pot -d resources/locales')
+    ctx.run('pybabel compile -d resources/locales --use-fuzzy')
+
+
+@duty
+def hello(ctx: Context):
+    """ Print out hello world """
+
+    print('Hello world')
+
+
+@duty
+def optimize(ctx: Context):
+    """ Optimize images """
+
+    ctx.run('optimize-images .')
