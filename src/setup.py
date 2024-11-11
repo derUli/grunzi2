bd93e531356964a15600bc92e331b770af0defb6
Uli <ulidots@ulidots.de>
2024-11-11 16:42:37 +0100

    Code quality

---
+++ src/setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+""" cx_freeze setup file """
+
+import glob
+import json
+import os
+import shutil
+import sys
+
+import cx_Freeze
+import xmlformatter
+
+target_name = 'amerre'
+base = None
+
+if sys.platform == 'win32':
+    target_name = 'Amerre.exe'
+
+target = cx_Freeze.Executable(
+    script="amerre.py",
+    icon=os.path.join(
+        os.path.dirname(__file__),
+        'resources',
+        'images',
+        'ui',
+        'icon.ico'
+    ),
+    base=base,
+    target_name=target_name
+)
+
+OPTIMIZE = 0
+
+if sys.platform == 'win32':
+    OPTIMIZE = 1
+
+options = {
+    'build_exe': {
+        # "include_msvcr": True, Not allowed to legal reasons
+        'optimize': OPTIMIZE,
+        'silent_level': 3,
+        'includes': [
+            'pyogg'
+        ],
+        'include_files': [
+            'resources/',
+            '../CREDITS.txt'
+        ],
+    }
+}
+
+cx_Freeze.setup(
+    name='Amerre',
+    options=options,
+    executables=[
+        target
+    ]
+)
