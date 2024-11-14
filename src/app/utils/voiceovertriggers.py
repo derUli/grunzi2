4880cf647ea6030843c1a5971e41ea97f3a4c72f
Uli <ulidots@ulidots.de>
2024-11-14 19:16:40 +0100

    Code quality

--- src/app/utils/voiceovertriggers.py
+++ src/app/utils/voiceovertriggers.py
@@ -24,7 +24,7 @@ class VoiceOverTiggers:
 
         voiceovers = []
 
-        for i in range(1, 5):
+        for i in range(1, 6):
             voiceovers.append("text" + str(i).rjust(2, '0') + '.mp3')
 
         random.shuffle(voiceovers)
