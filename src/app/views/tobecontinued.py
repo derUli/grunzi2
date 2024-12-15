b7fc4d2965acbdfdb40c77cdfdfe71f314f523e5
Uli <ulidots@ulidots.de>
2024-12-15 10:03:37 +0100

    Back to m

--- src/app/views/tobecontinued.py
+++ src/app/views/tobecontinued.py
@@ -4,6 +4,8 @@
 import arcade
 
 from app.constants.fonts import FONT_MARKER_FELT
+from app.constants.input.controllers import KEY_START
+from app.constants.input.keyboard import KEY_CONFIRM
 from app.views.view import View
 
 FONT_SIZE = 60
@@ -13,10 +15,13 @@ SCENE_LAYER_TEXT = 'Text'
 class ToBeContinued(View):
 
     def __init__(self):
+        """ To be continued screen """
+
+        super().__init__()
+
         self._text_completed = None
         self.window = None
         self.background_color = arcade.csscolor.WHITE
-    """ To be continued screen"""
 
     def setup(self, root_dir: str):
         """ Setup logo screen """
@@ -39,6 +44,13 @@ class ToBeContinued(View):
 
         return self
 
+    def on_key_press(self, symbol: int, modifiers: int):
+        """ Handle keyboard input """
+
+
+        if symbol in KEY_CONFIRM and self._fade_sprite is None:
+            self.on_main_menu()
+
     def on_update(self, delta_time: float):
         w, h = self.window.get_size()
         self._text_completed.center_x = w / 2
@@ -50,3 +62,15 @@ class ToBeContinued(View):
 
         self.clear()
         self._scene.draw()
+
+    def on_button_press(self, joystick, key) -> None:
+        """ On controller button press """
+
+        if key == KEY_START:
+            self.on_main_menu()
+
+
+
+    def on_main_menu(self):
+        from app.views.mainmenu import MainMenu
+        self.window.show_view(MainMenu().setup(self._root_dir))
